a6c59a3355d8ee0017edcf8a61dd698c5b110023
Mitchell Donahue <Mitchell.Donahue@wdc.com>
2018-07-02 16:25:26 -0700

    Initial Commit

---
+++ mitch/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mitch
+//
+//  Created by Mitchell Donahue on 7/2/18.
+//  Copyright © 2018 Mitchell Donahue. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
